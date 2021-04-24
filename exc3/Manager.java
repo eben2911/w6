34da6b94a3feb342341d07249de229e8b6156359
eben2911 <78593632+eben2911@users.noreply.github.com>
2021-04-24 23:00:00 +0700

    Add files via upload

---
+++ exc3/Manager.java
@@ -0,0 +1,26 @@
+package excercise3;
+
+import java.util.*;
+
+public class Manager extends Employee implements Sortable{
+	private String secretaryName;
+	
+	public Manager(String n, double s, int d, int m, int y){
+		super(n, s, d, m, y);
+		secretaryName = "";
+	}
+	
+	public String getSecretaryName(){
+		return secretaryName;
+	}
+	
+	public void raiseSalary(double byPercent){
+		// add 1/2% bonus for every year of service
+		GregorianCalendar todaysDate = new GregorianCalendar();
+		int currentYear = todaysDate.get(Calendar.YEAR);
+		double bonus = 0.5 * (currentYear - hireYear());
+		super.raiseSalary(byPercent + bonus);
+	}
+	
+	
+}
